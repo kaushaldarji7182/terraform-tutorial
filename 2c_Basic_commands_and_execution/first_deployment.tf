324850f2ebf5af617d70365c0d8e02e9b595b2ba
vilasvarghese <vilas.varghese@gmail.com>
2021-06-11 09:02:56 +0530

    latest update

--- 2c_Basic_commands_and_execution/first_deployment.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"
-  region     = "us-east-1"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-}
--- 2e_Providers/providers.tf
+++ 2e_Providers/providers.tf
@@ -1,9 +1,15 @@
+# The default provider configuration; resources that begin with `aws_` will use
+# it as the default, and it can be referenced as `aws`.
 provider "aws" {
   access_key = "ACCESS_KEY"
   secret_key = "SECRET_KEY"
   region     = "us-east-1"
 }
 
+
+# Additional provider configuration for west coast region; resources can
+# reference this as `aws.us-west-1`.
+# Further read: https://www.terraform.io/docs/language/providers/configuration.html
 provider "aws" {
   access_key = "ACCESS_KEY"
   secret_key = "SECRET_KEY"
