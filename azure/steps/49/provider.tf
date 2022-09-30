30041bb8d9266f10f3fb205a06bc47297bfce37e
vilasvarghese <vilas.varghese@gmail.com>
2022-09-30 05:59:20 +0530

    updates

--- azure/steps/49/provider.tf
+++ azure/steps/49/provider.tf
@@ -5,23 +5,11 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
       version = ">= 2.0" 
-    }
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.0"
-    }
+    }    
   }
 }
 
 # Provider Block
 provider "azurerm" {
  features {}          
-}
-
-# Random String Resource
-resource "random_string" "myrandom" {
-  length = 6
-  upper = false 
-  special = false
-  numeric = false   
 }
